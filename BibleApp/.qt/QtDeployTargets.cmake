9142a1b7e80ff28da4d149c38c5a5e92fada77fe
Cameron Coppoletta <ccoppoletta@outlook.com>
2025-03-26 22:20:04 -0400

    Uploading built and compiled .exe with zip for use by anyone who wants to read the Bible

---
+++ BibleApp/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_BibleAppGui_FILE C:/Users/Owner/Documents/BibleAppGui/build/Desktop_Qt_6_8_3_MinGW_64_bit-Release/BibleAppGui.exe)
+set(__QT_DEPLOY_TARGET_BibleAppGui_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_BibleAppGui_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
